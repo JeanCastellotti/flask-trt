ed5f5371a963bdd00a6d45ea17e6846547502ad1
JeanDevFR <castellotti.j@gmail.com>
2022-01-06 19:01:38 +0100

    Add SQLAlchemy and Alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config=Config):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.main.routes import main
 
