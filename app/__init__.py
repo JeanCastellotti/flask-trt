6016979df7dc5c8a29a0ce3f92c9e181a619e401
JeanDevFR <castellotti.j@gmail.com>
2022-01-09 09:15:22 +0100

    Add Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from app.config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 
 def create_app(config=Config):
@@ -17,11 +18,12 @@ def create_app(config=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    login_manager.init_app(app)
 
-    from app.main.routes import main
     from app.auth.routes import auth
+    from app.main.routes import main
 
-    app.register_blueprint(main)
     app.register_blueprint(auth)
+    app.register_blueprint(main)
 
     return app
