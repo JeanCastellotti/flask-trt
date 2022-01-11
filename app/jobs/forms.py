6cfe94ea3316348a814d880cbdc24879af6a26da
JeanDevFR <castellotti.j@gmail.com>
2022-01-11 21:28:52 +0100

    Update jobs

--- app/jobs/forms.py
+++ app/jobs/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, TextAreaField, SubmitField
+from wtforms.fields import StringField, TextAreaField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
 class CreateJobForm(FlaskForm):
     title = StringField("Titre", validators=[DataRequired(), Length(max=150)])
     place = StringField("Lieu", validators=[DataRequired(), Length(max=50)])
+    salary = IntegerField("Salaire", validators=[DataRequired()])
     description = TextAreaField("Description", validators=[DataRequired()])
     submit = SubmitField("Publier une annonce")
--- app/jobs/routes.py
+++ app/jobs/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 from .forms import CreateJobForm
 from app import db
 from app.models import Job
@@ -6,12 +7,25 @@ from app.models import Job
 jobs = Blueprint("jobs", __name__, url_prefix="/jobs")
 
 @jobs.route("/create", methods=["GET", "POST"])
+@login_required
 def create():
+    if not current_user.company or not current_user.address:
+        flash("Vous devez compléter votre profil avant de pouvoir publier une annonce.", "warning")
+        return redirect(url_for("main.home"))
     form = CreateJobForm()
     if form.validate_on_submit():
-        job = Job(title=form.title.data, place=form.place.data, description=form.description.data)
+        job = Job(title=form.title.data.strip(), 
+                  salary=form.salary.data.strip(), 
+                  place=form.place.data.strip(), 
+                  description=form.description.data.strip())
         db.session.add(job)
         db.session.commit()
         flash("Votre annonce a été créée.", "success")
         return redirect(url_for("main.home"))
     return render_template("jobs/create.html", form=form)
+
+
+@jobs.route("/<int:id>")
+def show(id):
+    job = Job.query.get_or_404(id)
+    return render_template("jobs/show.html", job=job)
