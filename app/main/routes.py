a817170daa806ad2a3d68b8513f0feee8c13eb41
JeanDevFR <castellotti.j@gmail.com>
2022-01-11 21:33:44 +0100

    Update home view

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from app.models import Job
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():    
-    return render_template("index.html")
+    jobs = Job.query.filter_by(is_active=True).order_by(Job.id.desc()).all()
+    return render_template("index.html", jobs=jobs)
