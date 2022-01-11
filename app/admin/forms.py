9372732832f8136d6e7ae39d1d8647321d391aac
JeanDevFR <castellotti.j@gmail.com>
2022-01-11 21:29:15 +0100

    Add email validation

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from app.models import User
 
 
 class CreateUserForm(FlaskForm):
@@ -8,3 +9,8 @@ class CreateUserForm(FlaskForm):
     password = PasswordField("Mot de passe", validators=[DataRequired(), Length(min=8)])
     password_confirm = PasswordField("Confirmation mot de passe", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Créer")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Cette adresse email n'est pas disponible")
