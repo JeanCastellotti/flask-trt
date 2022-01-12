40086ca95c86670876fb1b789c968f8dc6f5acfa
JeanDevFR <castellotti.j@gmail.com>
2022-01-12 21:36:03 +0100

    Update js assets

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -1,36 +1,20 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('modal', () => ({
         visible: false,
-        item: null,
-        type: null,
-        action: null,
-        open() {
+        message: null,
+        next: null,
+        open(e) {
+            this.message = e.target.dataset.message
+            this.next = e.target.dataset.next
             this.visible = true
         },
         close() {
             this.visible = false
-            this.item = null
-        },
-        activate(e) {
-            this.item = e.target.dataset.id
-            this.type = e.target.dataset.type
-            this.action = 'activate'
-            this.open()
-        },
-        deactivate(e) {
-            this.item = e.target.dataset.id
-            this.type = e.target.dataset.type
-            this.action = 'deactivate'
-            this.open()
-        },
-        deleteItem(e) {
-            this.item = e.target.dataset.id
-            this.type = e.target.dataset.type
-            this.action = 'delete'
-            this.open()
+            this.message = null
+            this.next = null
         },
         submit(e) {
-            e.target.action = `/admin/${this.action}/${this.type}s/${this.item}`
+            e.target.action = this.next
             e.target.submit()
         }
     }))
--- app/static/js/tabs.js
+++ app/static/js/tabs.js
@@ -1,26 +1,15 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('tabs', () => ({
-        activeTab: null,
+        tab: null,
         init() {
             const page = location.href.split('/').at(-1)
 
             if (!page) return
 
-            switch (page) {
-                case '':
-                case 'candidates':
-                    this.activeTab = 'tab1'
-                    break;
-                case 'recruiters':
-                    this.activeTab = 'tab2'
-                    break;
-                case 'consultants':
-                    this.activeTab = 'tab3'
-                    break;
-                case 'jobs':
-                    this.activeTab = 'tab4'
-                    break;
-            }
+            this.tab = page
+        },
+        isActive(tab) {
+            return this.tab === tab
         }
     }))
 })
