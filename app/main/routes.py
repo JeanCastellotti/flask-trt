e58ad33d5bfe75f459dccfa7816efaa4529a2685
JeanDevFR <castellotti.j@gmail.com>
2022-01-13 22:18:14 +0100

    Add pagination

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.models import Job
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():    
-    jobs = Job.query.filter_by(is_active=True).order_by(Job.id.desc()).all()
+    page = request.args.get("page", 1, type=int)
+    jobs = Job.query.filter_by(is_active=True).order_by(Job.id.desc()).paginate(page=page, per_page=5)
     return render_template("index.html", jobs=jobs)
