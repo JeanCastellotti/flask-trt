38fa20f36056516145bee338e4ffc1d158223f75
JeanDevFR <castellotti.j@gmail.com>
2022-01-16 16:02:08 +0100

    Update forms

--- app/account/forms.py
+++ app/account/forms.py
@@ -1,19 +1,24 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.fields import StringField, EmailField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+from app.models import User
 
-
-class UpdateAccountCandidateForm(FlaskForm):
+class UpdateAccountForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Mettre à jour")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Cette adresse email n'est pas disponible.")
+
+class UpdateAccountCandidateForm(UpdateAccountForm):
     first_name = StringField("Prénom", validators=[Length(max=50)])
     last_name = StringField("Nom", validators=[Length(max=50)])
     resume_file = FileField("CV", validators=[FileAllowed(["pdf"])])
-    submit = SubmitField("Mettre à jour")
 
 
-class UpdateAccountRecruiterForm(FlaskForm):
-    email = EmailField("Email", validators=[DataRequired(), Email()])
+class UpdateAccountRecruiterForm(UpdateAccountForm):
     company = StringField("Société", validators=[Length(max=50)])
     address = StringField("Adresse", validators=[Length(max=150)])
-    submit = SubmitField("Mettre à jour")
